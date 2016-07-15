092bbb9c474accf906dfb9a8c9bffa7f89d98435
Weslei Freitas <weslei.rfreitas@gmail.com>
2016-07-15 10:55:42 -0300

    feat(px-data-grid): adicionar componente px-data-grid

---
+++ src/system/components/px-data-grid/px-data-grid.filter.js
@@ -0,0 +1,12 @@
+angular.module('px-data-grid', [])
+	.filter('dataGridRefresh', [function() {
+		return function(working) {
+			// Verifica working da px-data-grid
+			// Class no botão Atualizar (Listagem)
+			if (working) {
+				return 'fa fa-refresh fa-spin';
+			} else {
+				return 'fa fa-refresh';
+			}
+		}
+	}]);
---
+++ src/system/components/px-data-grid/px-data-grid.service.js
@@ -0,0 +1,66 @@
+angular.module('px-data-grid.service', [])
+    .factory('pxDataGridService', pxDataGridService);
+
+pxDataGridService.$inject = ['pxConfig', '$http', '$rootScope'];
+
+function pxDataGridService(pxConfig, $http, $rootScope) {
+    var service = {};
+
+    service.select = select;
+    service.remove = remove;
+
+    return service;
+
+    function select(data, callback) {
+
+        data.dsn = pxConfig.PROJECT_DSN;
+        data.cfcPath = pxConfig.PX_CFC_PATH;
+
+        if ($rootScope.hasOwnProperty('globals.currentUser.userId')) {
+            data.user = $rootScope.globals.currentUser.userId;
+        }
+
+        if (!angular.isDefined(data.orderBy)) {
+            data.orderBy = '';
+        }
+
+        if (!angular.isDefined(data.groupItem)) {
+            data.groupItem = '';
+        }
+
+        if (!angular.isDefined(data.groupLabel)) {
+            data.groupLabel = '';
+        }
+
+        if (!angular.isDefined(data.where)) {
+            data.where = '';
+        }
+
+        $http({
+            method: 'POST',
+            url: '../../../rest/px-project/system/px-data-grid/getData',
+            data: data
+        }).then(function successCallback(response) {
+            callback(response);
+        }, function errorCallback(response) {
+            alert('Ops! Ocorreu um erro inesperado.\nPor favor contate o administrador do sistema!');
+        });
+    }
+
+    function remove(data, callback) {
+
+        data.dsn = pxConfig.PROJECT_DSN;
+        data.cfcPath = pxConfig.PX_CFC_PATH;
+        data.user = $rootScope.globals.currentUser.usu_id;
+
+        $http({
+            method: 'POST',
+            url: '../../../rest/px-project/system/px-data-grid/removeData',
+            data: data
+        }).then(function successCallback(response) {
+            callback(response);
+        }, function errorCallback(response) {
+            alert('Ops! Ocorreu um erro inesperado.\nPor favor contate o administrador do sistema!');
+        });
+    }
+}
