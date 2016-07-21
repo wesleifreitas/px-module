df57db2939f52522f53cf9cb2b77ad9953b18e59
Weslei Freitas <weslei.rfreitas@gmail.com>
2016-07-21 08:23:50 -0300

    feat(demo): atualizar demo
    Adicionar link e evento px-item-click

--- demo/px-data-grid/app.js
+++ demo/px-data-grid/app.js
@@ -2,74 +2,88 @@ var app = angular.module('demo', ['pxConfig', 'px-data-grid']);
 
 app.config(function(pxConfig) {
 
-  // Definir language da lib numeral.js
-  // http://numeraljs.com/                                                               
-  numeral.language('pt-br');
-  // Definir locale da lib moment.js
-  // http://momentjs.com/docs/
-  moment.locale('pt-BR');
+    // Definir language da lib numeral.js
+    // http://numeraljs.com/
+    numeral.language('pt-br');
+    // Definir locale da lib moment.js
+    // http://momentjs.com/docs/
+    moment.locale('pt-BR');
 });
 
 app.controller('DataGridCtrl', ['$scope', '$locale', function($scope, $locale) {
-  /**
-   * Controle da listagem
-   * Note que a propriedade 'control' da directive px-data-grid é igual a 'dgExemplo'
-   * Exemplo: <px-data-grid px-control='dgExemplo'>
-   * @type {Object}
-   */
-  $scope.dgExemploControl = {};
-
-  /**
-   * Inicializa listagem
-   * @return {Void}
-   */
-  $scope.dgExemploInit = function() {
     /**
-     * Configurações da listagem
-     * - fields: Colunas da listagem
+     * Controle da listagem
+     * Note que a propriedade 'control' da directive px-data-grid é igual a 'dgExemplo'
+     * Exemplo: <px-data-grid px-control='dgExemplo'>
      * @type {Object}
      */
-    $scope.dgExemploConfig = {
-      url: 'data.json',
-      fields: [{
-        title: 'Nº',
-        field: 'id',
-        type: 'int'
-      }, {
-        title: 'Nome',
-        field: 'nome',
-        type: 'varchar'
-      }, {
-        title: 'CPF',
-        field: 'cpf',
-        type: 'varchar',
-        stringMask: '###.###.###-##'
-      }, {
-        title: 'Data',
-        field: 'data',
-        type: 'date',
-        moment: 'dddd - DD/MM/YYYY'
-      }]
-    };
+    $scope.dgExemploControl = {};
 
-    $scope.getData = function() {
-      $scope.dgExemploControl.getData();
+    /**
+     * Inicializa listagem
+     * @return {Void}
+     */
+    $scope.dgExemploInit = function() {
+        /**
+         * Configurações da listagem
+         * - fields: Colunas da listagem
+         * @type {Object}
+         */
+        $scope.dgExemploConfig = {
+            url: 'data.json',
+            fields: [{
+                link: true,
+                linkId: "sampleIcon",
+                title: "",
+                class: "material-icons",
+                icon: "done",
+                value: ""
+            }, {
+                title: 'Nº',
+                field: 'id',
+                type: 'int'
+            }, {
+                title: 'Nome',
+                field: 'nome',
+                type: 'varchar'
+            }, {
+                title: 'CPF',
+                field: 'cpf',
+                type: 'varchar',
+                stringMask: '###.###.###-##'
+            }, {
+                title: 'Data',
+                field: 'data',
+                type: 'date',
+                moment: 'dddd - DD/MM/YYYY'
+            }]
+        };
+
+        $scope.getData = function() {
+            $scope.dgExemploControl.getData();
+        }
+
+        $scope.countRow = 1;
+        $scope.addDataRow = function() {
+
+            $scope.dgExemploControl.addDataRow({
+                "id": $scope.countRow++,
+                "nome": "Nome " + Math.floor((Math.random() * 100000) + 1),
+                "cpf": Math.floor((Math.random() * 10000000000) + 1000000000),
+                "data": new Date()
+            })
+
+        }
+
+        $scope.removeRow = function() {
+            $scope.dgExemploControl.removeRow('.selected');
+        }
+
+        $scope.itemClick = function(event) {
+            console.info(event.itemClick);
+            if (event.itemClick.pxLink.link && event.itemClick.pxLink.linkId === 'sampleIcon') {
+                alert('Você clicou no ícone')
+            }
+        }
     }
-
-    $scope.countRow = 1;
-    $scope.addDataRow = function() {
-
-      $scope.dgExemploControl.addDataRow({
-        "id": $scope.countRow++,
-        "nome": "Nome " + Math.floor((Math.random() * 100000) + 1),
-        "cpf": Math.floor((Math.random() * 10000000000) + 1000000000),
-        "data": new Date()
-      })
-
-    }
-
-    $scope.removeRow = function() {
-      $scope.dgExemploControl.removeRow('.selected');
-    }
-  };
 }]);
