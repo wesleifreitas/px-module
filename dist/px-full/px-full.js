4eaf43cf4c051c8cc6982fa8627780ea149b2817
Weslei Freitas <weslei.rfreitas@gmail.com>
2016-07-26 11:42:42 -0300

    chore(dist): atualizar dist

--- dist/px-full/px-full.js
+++ dist/px-full/px-full.js
@@ -2173,6 +2173,9 @@ module.directive('pxDataGrid', ['pxConfig', 'pxArrayUtil', 'pxUtil', '$timeout',
                             "orderable": false,
                             "className": "dt-body-center",
                             "render": function(data, type, full, meta) {
+                                if (typeof index.icon === 'undefined') {
+                                    index.icon = '';
+                                }
                                 return "<div class='link'><i link='true' linkId=" + index.linkId + " class='" + index.class + "'>" + index.icon + "</i></div>";
                             }
                         });
@@ -2184,10 +2187,7 @@ module.directive('pxDataGrid', ['pxConfig', 'pxArrayUtil', 'pxUtil', '$timeout',
                             "searchable": false,
                             "orderable": false,
                             "className": "dt-body-center",
-                            "render": function(data, type, full, meta) {
-                                if (typeof index.icon === 'undefined') {
-                                    index.icon = '';
-                                }
+                            "render": function(data, type, full, meta) {                                
                                 return data;
                             }
                         });
