f8ce09b28666a085d7987ce08e15b1e1a2faa776
Weslei Freitas <weslei.rfreitas@gmail.com>
2016-07-28 15:27:19 -0300

    fix(px-form-item): ajustar função setDefault

--- src/system/components/px-form-item/px-form-item.js
+++ src/system/components/px-form-item/px-form-item.js
@@ -1223,6 +1223,9 @@ angular.module('px-form-item', ['ui.mask'])
 
 				$scope.setDefault = function(data) {
 					$scope.default = data;
+					$timeout(function() {
+						$scope.oldValue = data[$scope.fields[pxArrayUtil.getIndexByProperty($scope.fields, 'labelField', true)].field];
+					}, 0)
 				}
 
 				$scope.setValue = function(data) {
