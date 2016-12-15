ea726e3e4ee09fccc31cc9254c8a565884669d47
Weslei Freitas <weslei.rfreitas@gmail.com>
2016-12-15 11:11:00 -0200

    style(px-data-grid): ajustar código

--- src/system/components/px-data-grid/px-data-grid.js
+++ src/system/components/px-data-grid/px-data-grid.js
@@ -256,7 +256,7 @@ module.directive('pxDataGrid', ['pxConfig', 'pxArrayUtil', 'pxUtil', '$timeout',
                     }
 
                     // Verificar se o campo é link
-                    if (index.link || index.class) {                        
+                    if (index.link || index.class) {
                         index.width = '' || '1%';
                         scope.links[columnDefs] = index;
                         scope.columnDefs.push({
@@ -271,7 +271,29 @@ module.directive('pxDataGrid', ['pxConfig', 'pxArrayUtil', 'pxUtil', '$timeout',
                                 }
                                 if (index.link) {
                                     var value = index.value === false ? '' : data;
-                                    return "<div class='link " + index.align + "'><i link='true' linkId=" + index.linkId + " class='" + index.class + "'>" + index.icon + "</i>" + value + "</div>";
+
+                                    // Retornar directive
+                                    /*var html = "<div class='link " +
+                                        index.align +
+                                        "'><i link='true' linkId=" +
+                                        index.linkId + " class='" +
+                                        index.class + "'>" + index.icon +
+                                        "</i>" +
+                                        value +
+                                        "</div>";
+                                    return ($compile(angular.element(html))(scope)[0]).innerHTML;*/
+
+                                    return "<div class='link " +
+                                        index.align +
+                                        "'><i link='true' linkId=" +
+                                        index.linkId +
+                                        " class='" +
+                                        index.class +
+                                        "'>" +
+                                        index.icon +
+                                        "</i>" +
+                                        value +
+                                        "</div>";
                                 } else {
                                     return "<div class='" + index.align + "'><i class='" + index.class + "'>" + index.icon + "</i></div>";
                                 }
